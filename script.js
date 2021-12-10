40aafc665121a8e9222d412ddf8ec8ed466517ee
Jasper Oudemans <jasperoudemans@gmail.com>
2021-12-09 22:49:25 -0700

    got the password to display in the password box

--- script.js
+++ script.js
@@ -8,12 +8,30 @@ var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '#', '$', '%', '&', '*'];
 var word = [];
 
+//function to get a random number.
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.ceil(max);
   return Math.floor(Math.random()*(max-min) + min);
 }
 
+//declaring function/variable pairs to generate an index number to apply to each array of characters.
+var lowSelIndex = getRandomInt(0, lowerCase.length);
+  var lowSel = lowerCase[lowSelIndex];
+
+
+  var upSelIndex = getRandomInt(0, upperCase.length);
+  var upSel = upperCase[upSelIndex];
+
+  var numSelIndex = getRandomInt(0, numeric.length);
+  var numSel = numeric[numSelIndex];
+
+  var specSelIndex = getRandomInt(0, special.length);
+  var specSel = special[specSelIndex];
+
+
+  //This is where the window prompts, confirms, and alerts are. 
+  //Function to generate password.
 function generatePassword() {
   numberOfChars = window.prompt("Enter a number of characters for your password between 8 and 128:"); 
    if(numberOfChars >= 8 && numberOfChars <= 128) {
@@ -31,24 +49,15 @@ function generatePassword() {
     charsToInclude[3] = window.confirm("Include special characters?");
   }
   if(charsToInclude[3] == true || charsToInclude[3] == false) {
-    window.alert(charsToInclude);
+    window.alert("The boolean values of your parameter choices were " + charsToInclude[0] + ", " + 
+    charsToInclude[1] + ", " + charsToInclude[2] + ", " + charsToInclude[3] + ". Unfortunately, " + 
+    "I can only display the number of characters you requested minus one as commas. Your entry will generate " + numberOfChars + 
+    " commas minus one.");
   }
-  var lowSelIndex = getRandomInt(0, lowerCase.length);
-  var lowSel = lowerCase[lowSelIndex];
-
-  var upSelIndex = getRandomInt(0, upperCase.length);
-  var upSel = upperCase[upSelIndex];
-
-  var numSelIndex = getRandomInt(0, numeric.length);
-  var numSel = numeric[numSelIndex];
-
-  var specSelIndex = getRandomInt(0, special.length);
-  var specSel = special[specSelIndex];
 
   word.length = numberOfChars;
   
-
-  return window.alert("You're password is: " + word);
+  return word;
   }
 
 
@@ -57,7 +66,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = word;
 
 }
 
